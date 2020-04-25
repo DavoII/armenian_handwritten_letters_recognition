0c75240f03c26b7340ebd6a62af8326f98d47305
DavoII <davo.yerits@gmail.com>
2020-04-25 18:41:16 +0400

    added loading for lowercase data

--- load_data_and_lables_into_files.py
+++ load_data_and_lables_into_files.py
@@ -1,6 +1,7 @@
+import sys
 import glob
-import PIL
-import cv2
+from PIL import Image
+#import cv2
 import numpy as np
 
 from keras.utils import np_utils
@@ -19,21 +20,56 @@ lower_case_class_idx = [
 
 data = []
 labels = []
+uppercase_data = []
+uppercase_labels = []
+lowercase_data = []
+lowercase_labels = []
 
-def getFIlePath(className):
-    return './dataset/Upper cleaned/Upper cleaned/' + className + '/*.png'
+def getFIlePath(folderName, className):
+    return './' + folderName +  '/' + className + '/*.png'
 
-for index, className in enumerate(upper_case_class_idx):
-    for idx, fileName in enumerate(glob.glob(getFIlePath(className))):
-        print(className, idx)
-        img = np.array(PIL.Image.open(fileName))
-        median = []
-        median = cv2.fastNlMeansDenoising(img)
-        data.append(median)
-        labels.append(index)
+
+def load_data(args):
+        if '-u' in args:
+            folderName = 'Upper cleaned'
+            for index, className in enumerate(upper_case_class_idx):
+                for idx, fileName in enumerate(glob.glob(getFIlePath(folderName, className))):
+                    print(className, idx)
+                    img = np.array(Image.open(fileName))
+                    #median = []
+                    #median = cv2.fastNlMeansDenoising(img)
+                    uppercase_data.append(img)
+                    uppercase_labels.append(index)
         
-data = np.array(data)
-labels = np_utils.to_categorical(labels, len(upper_case_class_idx))
+        if '-l' in args:
+            folderName = 'Lower cleaned'
+            for index, className in enumerate(lower_case_class_idx):
+                for idx, fileName in enumerate(glob.glob(getFIlePath(folderName, className))):
+                    print(className, idx)
+                    img = np.array(Image.open(fileName))
+                    #median = []
+                    #median = cv2.fastNlMeansDenoising(img)
+                    lowercase_data.append(img)
+                    lowercase_labels.append(index)
+
+system_arg = sys.argv
+system_arg.pop(0)
+
+if not system_arg:
+    load_data(['-l', '-u'])
+elif len(system_arg) == 1 and '-a'in system_arg:
+    load_data(['-l', '-u', '-a'])
+else: 
+    load_data(system_arg)
+        
+uppercase_data = np.array(uppercase_data)
+lowercase_data = np.array(lowercase_data)
+uppercase_labels = np_utils.to_categorical(uppercase_labels, len(upper_case_class_idx))
+lowercase_labels = np_utils.to_categorical(lowercase_labels, len(lower_case_class_idx))
+        
+data = np.concatenate(uppercase_data, lowercase_data)
+labels = np.concatenate(uppercase_labels, lowercase_labels)
+
 
 np.save('./dataset_v2/data.npy', data)
 np.save('./dataset_v2/labels.npy', labels)
